531772c13bf39bffac411c3fb251da064e0e49ef
Kpearson72 <kpearson72@gmail.com>
2021-03-23 14:05:35 -0500

    change to circle marker

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -28,5 +28,17 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.ge
 // Read the json using D3
 d3.json(url, function (geojson) {
     console.log(geojson);
-    L.geoJSON(geojson.features).addTo(myMap)
-});
+
+    // add style to my earthquake features
+    // function earthquakeStyle(features)
+    L.geoJSON(geojson, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        },  
+        
+        
+        
+        
+    }).addTo(myMap)
+
+})
