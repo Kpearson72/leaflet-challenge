db3d3c88fd9eb11e354cfd4611af0f60eb741437
Kpearson72 <kpearson72@gmail.com>
2021-03-23 12:45:08 -0500

    add json url to retrieve data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,5 @@
-// Define streetmap and darkmap layers
-let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Define worldMap tile layer
+let worldMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -8,7 +8,7 @@ let streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     accessToken: API_KEY
 });
 
-// Create our map, giving it the streetmap and earthquakes layers to display on load
+// define the myMap with center coords and zoom
 let myMap = L.map("mapid", {
     center: [
         37.10, -95.72
@@ -17,5 +17,6 @@ let myMap = L.map("mapid", {
 
 });
 
-streetmap.addTo(myMap);
+worldMap.addTo(myMap);
 
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
